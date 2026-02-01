c0d5b824a1eeec54683494288c6502b353418856
Finly-art <finly@tutamail.com>
2026-02-01 16:54:08 +0100

    fix server listen port

--- index.js
+++ index.js
@@ -43,5 +43,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on", PORT));
